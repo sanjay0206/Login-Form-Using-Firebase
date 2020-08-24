7cf2730f7c1a34d57e9e3172a8e3941537e16915
sanjay0206 <62499148+sanjay0206@users.noreply.github.com>
2020-08-24 14:56:14 +0530

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,29 @@
+// Your web app's Firebase configuration
+
+var firebaseConfig = {
+    apiKey: "AIzaSyD1ttF-Blf-slZk7QbsGtWzWbW0vKqVhAA",
+    authDomain: "login-form-35bfb.firebaseapp.com",
+    databaseURL: "https://login-form-35bfb.firebaseio.com",
+    projectId: "login-form-35bfb",
+    storageBucket: "login-form-35bfb.appspot.com",
+    messagingSenderId: "299672928059",
+    appId: "1:299672928059:web:c9a77438fb5d9e9618c11e",
+    measurementId: "G-P6PXC1GHR1"
+  };
+
+
+
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+  const auth = firebase.auth();
+
+  function signIn(){
+      var email = document.getElementById("email");
+      var password = document.getElementById("password");
+
+      const promise = auth.createUserWithEmailAndPassword(email.value,password.value);
+      promise.catch(e => alert(e.message));
+      alert("Signed In");
+
+  }
